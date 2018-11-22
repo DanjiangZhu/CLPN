8732c634dd63de1cc3faef6004e0d7698fc5d2b1
Danjiang Zhu <zhudanjiang@hotmail.com>
2018-11-22 15:05:46 +0800

    PIPE原始版本

---
+++ PIPE-4.3.2/src/main/java/Pipe.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pipe.controllers.PipeApplicationController;
+import pipe.models.PipeApplicationModel;
+
+import javax.swing.*;
+public class Pipe
+{
+
+	private PipeApplicationModel applicationModel;
+	@SuppressWarnings("unused")
+	private PipeApplicationController applicationController;
+
+    private static final Logger logger= LogManager.getLogger(Pipe.class);
+
+	private Pipe(String version)
+    {
+        logger.trace("初始化Pipe");
+
+
+        applicationModel = new PipeApplicationModel(version);
+        applicationController = new PipeApplicationController(applicationModel);
+    }
+    public static void main(String args[])
+    {
+        Runnable runnable = pipeRunnable();
+        SwingUtilities.invokeLater(runnable);
+    }
+	protected static Runnable pipeRunnable()
+	{
+		Runnable runnable = new Runnable()
+                            {
+                                public void run()
+                                {
+                                    @SuppressWarnings("unused")
+									Pipe pipe = new Pipe("v4.3.0");
+                                }
+                            };
+		return runnable;
+	}
+    protected static void runPipeForTesting() throws InterruptedException, InvocationTargetException
+    {
+    	SwingUtilities.invokeAndWait(pipeRunnable()); 
+    }
+}
